2f6e3bbc2bbaa4721bd108ee696c139981d9a913
FFuuZZuu <47675688+FFuuZZuu@users.noreply.github.com>
2020-05-19 16:17:43 +0100

    Working Demo

---
+++ Lift Project/Assets/Scriptss/ClampElevatorToCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClampElevatorToCamera : MonoBehaviour
+{
+    public Transform target;
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPos = target.position;
+
+        targetPos.x = transform.position.x;
+    }
+}
---
+++ Lift Project/Assets/Scriptss/ElevatorMoveOnSpace.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ElevatorMoveOnSpace : MonoBehaviour
+{
+    public InputActionMap controls;
+
+    public float elavatorRaiseAmount = 3;
+
+    void Awake()
+    {
+        controls["Floor Up"].performed += _ => FloorUp();
+        controls["Floor Down"].performed += _ => FloorDown();
+    }
+
+    void FloorUp()
+    {
+        transform.Translate(new Vector3(0, elavatorRaiseAmount, 0), Space.World);
+    }
+
+    void FloorDown()
+    {
+        transform.Translate(new Vector3(0, -elavatorRaiseAmount, 0), Space.World);
+    }
+
+    void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Disable();
+    }
+}
